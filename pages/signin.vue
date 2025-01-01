2022c9b6561ef7f52a1b07719420965c605e4b05
Riceball <42161586+riceball-tw@users.noreply.github.com>
2025-01-02 05:45:58 +0800

    feat: Add sign in and sign up page

---
+++ pages/signin.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div class="h-screen flex justify-center items-center">
+    <Card class="mx-auto max-w-sm">
+    <CardHeader>
+      <CardTitle class="text-xl">
+        Sign in
+      </CardTitle>
+      <CardDescription>
+        Enter your information to sign in an account
+      </CardDescription>
+    </CardHeader>
+    <CardContent>
+      <div class="grid gap-4">
+        <div class="grid gap-2">
+          <Label for="email">Email</Label>
+          <Input
+            id="email"
+            type="email"
+            required
+          />
+        </div>
+        <div class="grid gap-2">
+          <Label for="password">Password</Label>
+          <Input id="password" type="password" />
+        </div>
+        <Button type="submit" class="w-full">
+          Sign in an account
+        </Button>
+      </div>
+      <div class="mt-4 text-center text-sm">
+        Don't have an account?
+        <NuxtLink class="underline" to="/signup">Sign up</NuxtLink>
+      </div>
+    </CardContent>
+  </Card>
+  </div>
+</template>
---
+++ pages/signup.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="h-screen flex justify-center items-center">
+    <Card class="mx-auto max-w-sm">
+    <CardHeader>
+      <CardTitle class="text-xl">
+        Sign Up
+      </CardTitle>
+      <CardDescription>
+        Enter your information to create an account
+      </CardDescription>
+    </CardHeader>
+    <CardContent>
+      <form class="grid gap-4" @submit.prevent="handleSubmitSignUpForm">
+        <div class="grid gap-2">
+          <Label for="email">Email</Label>
+          <Input
+            id="email"
+            type="email"
+            required
+          />
+        </div>
+        <div class="grid gap-2">
+          <Label for="password">Password</Label>
+          <Input id="password" type="password" />
+        </div>
+        <Button type="submit" class="w-full">
+          Create an account
+        </Button>
+      </form>
+      <div class="mt-4 text-center text-sm">
+        Already have an account?
+        <NuxtLink class="underline" to="/signin">Sign in</NuxtLink>
+      </div>
+    </CardContent>
+  </Card>
+  </div>
+</template>
