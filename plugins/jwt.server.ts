0bf0b3d688ef6d907c63cb760e02c3116974da91
Riceball <42161586+riceball-tw@users.noreply.github.com>
2025-01-02 19:16:01 +0800

    feat: Add jwt verify nuxt server plugin

---
+++ plugins/jwt.server.ts
@@ -0,0 +1,12 @@
+import type { PublicKeyInput, JsonWebKeyInput } from 'crypto'
+import jwt from 'jsonwebtoken'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  return {
+    provide: {
+      verifyJwtToken: (token: string, secret: jwt.Secret | PublicKeyInput | Buffer<ArrayBufferLike> | JsonWebKeyInput, options?: jwt.VerifyOptions & { complete: true }) => {
+        return jwt.verify(token, secret, options)
+      }
+    }
+  }
+})
