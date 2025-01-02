e48169b261bc8eaba41b497d46daa438ff45ff00
Riceball <42161586+riceball-tw@users.noreply.github.com>
2025-01-02 18:12:29 +0800

    feat: Add slat colum in usersTable

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,4 +4,5 @@ export const usersTable = mysqlTable('user', {
   id: serial().primaryKey(),
   email: varchar({ length: 255 }).notNull().unique(),
   password: varchar('password', { length: 255 }).notNull(),
+  salt: varchar({length: 255}).notNull()
 });
