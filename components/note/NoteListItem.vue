420a8a46a5799515db937ea8d45f43a5b7372963
Riceball <42161586+riceball-tw@users.noreply.github.com>
2025-01-07 01:00:59 +0800

    fix: Avoid note list item too long, clamp texts to 1 line

--- components/note/NoteListItem.vue
+++ components/note/NoteListItem.vue
@@ -15,15 +15,13 @@
 
 <template>
   <li>
-    <button :disabled="isActive" :class="isActive ? 'bg-primary' : 'hover:bg-primary/60'" class=" p-4 flex w-full gap-2 flex-col items-start" type="button" @click="() => emit('change-current-note-id', noteId)">
-      <div v-if="title">
-        {{ title }}
-      </div>
+    <button :disabled="isActive" :class="isActive ? 'bg-primary' : 'hover:bg-primary/60'" class="p-4 flex w-full gap-2 flex-col items-start text-left" type="button" @click="() => emit('change-current-note-id', noteId)">
+      <div class="line-clamp-1" v-if="title">{{ title }}</div>
       <div v-else>
         Untitled
       </div>
 
-      <span class="text-xs text-muted-foreground">
+      <span class="text-xs text-muted-foreground line-clamp-1">
         {{ subTitle }}
       </span>
     </button>
