79c0e8978ccc5ea626dfda44a046af570da365fc
Riceball <42161586+riceball-tw@users.noreply.github.com>
2025-01-09 13:31:37 +0800

    feat: Add drizzle seed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/mysql2';
-  
+import { reset } from "drizzle-seed";
+import * as schema from "@/src/db/schema";
+
 export const db = drizzle(process.env.DATABASE_URL!);
+
+
+async function main() {
+  await reset(db, schema);
+}
+
+main()
